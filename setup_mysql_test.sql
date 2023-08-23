01a3102519b2f4ec69cc33f0a7a9e0d97639b85c
wosohd <wosohd@gmail.com>
2023-08-23 00:31:46 -0700

    -m SQL & models update

---
+++ setup_mysql_test.sql
@@ -0,0 +1,13 @@
+-- prepares a MySQL server for the HBNB project
+-- create project developement database with the name hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- creates a new user named hbnb_dev
+-- with the password hbnb_dev_pwd if it dosen't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- giving access all privileges to the new user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+-- giving access to SELECT privilege for the user hbnb_dev db performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+
