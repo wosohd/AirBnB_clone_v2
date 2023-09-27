0667ffb611502d558f5493f498712769ea94d111
okiogutu <okiogutu@gmail.com>
2023-09-27 11:12:28 -0700

    commss

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 <<<<<<< HEAD
-"""A script that starts a flask web application that listens on 0.0.0.0, port 5000"""
+"""A script that starts a flask web application"""
 
 from flask import Flask
 
@@ -16,9 +16,7 @@ def hello():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
 =======
-"""starts a flask web application
-listening on 0.0.0.0 port 5000
-"""
+"""starts a flask web application"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -26,11 +24,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_flask():
-    """Return specific string
-    """
+    """Return specific string"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
     app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
->>>>>>> ee7d4282272059bdc4332e07909ded1079c88667
